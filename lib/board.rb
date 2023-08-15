a786816242ff60a5abe7052ed9755b6d6ba3367c
Johan Watelet <johanwatelet@gmail.com>
2023-08-15 16:52:28 +0200

    add drawing algorithm

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,30 @@
 class Board
+  HEADER = " 1 2 3 4 5 6 7\n".freeze
+
+  def initialize
+    @array = initialize_array
+  end
+
   def draw
-    puts <<~BOARD
-       1 2 3 4 5 6 7
-      |_ _ _ _ _ _ _|
-      |_ _ _ _ _ _ _|
-      |_ _ _ _ _ _ _|
-      |_ _ _ _ _ _ _|
-      |_ _ _ _ _ _ _|
-      |_ _ _ _ _ _ _|
-    BOARD
+    puts HEADER + draw_body
+  end
+
+  private
+
+  def initialize_array
+    [
+      %w[_ _ _ _ _ _ _],
+      %w[_ _ _ _ _ _ _],
+      %w[_ _ _ _ _ _ _],
+      %w[_ _ _ _ _ _ _],
+      %w[_ _ _ _ _ _ _],
+      %w[_ _ _ _ _ _ _]
+    ]
+  end
+
+  def draw_body
+    @array.map do |line|
+      "|#{line.join(' ')}|"
+    end.join("\n")
   end
 end
